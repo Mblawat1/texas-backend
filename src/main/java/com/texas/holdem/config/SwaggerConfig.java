2a0e258f127a69bd2db1347be7ebc42e9594de86
Marek Bławat <45942167+Mblawat1@users.noreply.github.com>
2020-11-19 13:35:06 +0100

    swagger config

--- src/main/java/com/texas/holdem/config/SwaggerConfig.java
+++ src/main/java/com/texas/holdem/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +18,7 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .enableUrlTemplating(true)
                 .pathMapping("/");
     }
 }
