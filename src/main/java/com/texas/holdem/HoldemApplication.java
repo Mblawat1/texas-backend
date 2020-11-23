7c541d0f767bbf8769a5a136fc8cdae9a31dbced
thatstrangerwho <itrazjel@gmail.com>
2020-11-23 21:49:07 +0100

    CommunitySet improved, first hand checks

--- src/main/java/com/texas/holdem/HoldemApplication.java
+++ src/main/java/com/texas/holdem/HoldemApplication.java
@@ -1,15 +1,19 @@
 package com.texas.holdem;
 
+import com.texas.holdem.elements.Card;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 @SpringBootApplication
 public class HoldemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoldemApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/texas/holdem/logic/HandChecker.java
+++ src/main/java/com/texas/holdem/logic/HandChecker.java
@@ -1,8 +1,12 @@
 package com.texas.holdem.logic;
 
+import com.texas.holdem.elements.Card;
 import com.texas.holdem.elements.HoleSet;
 import com.texas.holdem.elements.CommunitySet;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class HandChecker {
@@ -28,11 +32,77 @@ public class HandChecker {
 
     public int checkHand(HoleSet holeSet, CommunitySet communitySet) {
         int hand = 0;
+        Card highCard = checkHighCard(holeSet, communitySet);
+        ArrayList<Card> set = makeSet(holeSet, communitySet);
         return hand;
     }
 
-    public boolean checkRoyalFlush(HoleSet holeSet, CommunitySet communitySet) {
-        return true;
+    public boolean checkRoyalFlush(ArrayList<Card> set) {
+
+        return false;
+    }
+
+    public boolean checkStraightFlush(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkFour(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkFullHouse(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkFlush(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkStraight(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkThree(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkTwoPairs(ArrayList<Card> set) {
+        return false;
+    }
+
+    public boolean checkPari(ArrayList<Card> set) {
+        return false;
+    }
+
+    public Card checkHighCard(HoleSet holeSet, CommunitySet communitySet) {
+        Card highCard = new Card();
+        for (Card c: communitySet.getCommunitySet()) {
+            if (highCard.getNumber() <= c.getNumber()) {
+                highCard = c;
+            }
+        }
+        if (highCard.getNumber() <= holeSet.getHoleCard1().getNumber()) {
+            highCard = holeSet.getHoleCard1();
+        }
+        if (highCard.getNumber() <= holeSet.getHoleCard2().getNumber()) {
+            highCard = holeSet.getHoleCard2();
+        }
+        return highCard;
+    }
+
+    public ArrayList<Card> makeSet(HoleSet holeSet, CommunitySet communitySet) {
+        ArrayList<Card> set = new ArrayList<>(7);
+        set.add(holeSet.getHoleCard1());
+        set.add(holeSet.getHoleCard2());
+        for (Card c: communitySet.getCommunitySet()) {
+            set.add(c);
+        }
+        Collections.sort(set, new Comparator<Card>() {
+            public int compare(Card c1, Card c2) {
+                return Integer.valueOf(c2.getNumber()).compareTo(c1.getNumber());
+            }
+        });
+        return set;
     }
 
 }
