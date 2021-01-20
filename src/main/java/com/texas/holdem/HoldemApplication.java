a9acdefc8a0448f724f3c305607a00903cb02fb9
thatstrangerwho <itrazjel@gmail.com>
2021-01-21 00:01:30 +0100

    counting sets of 3 and 4 in a hand added, sets of 4 to be improved

--- src/main/java/com/texas/holdem/HoldemApplication.java
+++ src/main/java/com/texas/holdem/HoldemApplication.java
@@ -6,12 +6,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 @SpringBootApplication
 public class HoldemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HoldemApplication.class, args);
+        ArrayList<Card> test = new ArrayList<>();
+        test.add(new Card(9, "sada"));
+        test.add(new Card(10, "sadna"));
+        test.add(new Card(2, "ahbdj"));
+        test.add(new Card(2, "sada"));
+        test.add(new Card(3, "sada"));
+        test.add(new Card(2, "sada"));
+        test.add(new Card(2, "sada"));
+        test.add(new Card(4, "sada"));
+        test.add(new Card(5, "sada"));
+        test.add(new Card(9, "sada"));
+        test.add(new Card(9, "sada"));
+        test.add(new Card(10, "sada"));
+        test.add(new Card(10, "sadajb"));
+        test.add(new Card(10, "sadajh"));
+        test.add(new Card(5, "sbhsb"));
+
+        SetCombsCounter counter = new SetCombsCounter();
+        System.out.println(counter.getPairCount(test));
+        System.out.println(counter.getTriplesCount(test));
+        System.out.println(counter.getQuadsCount(test));
 
     }
 
